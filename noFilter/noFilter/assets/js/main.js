e9e3c0b081d344b0620c144ed417c77f4c2106d5
pratra <pratra@github.com>
2021-06-20 04:45:02 -0400

    moved files to root

---
+++ noFilter/noFilter/assets/js/main.js
@@ -0,0 +1,218 @@
+
+(function() {
+  "use strict";  
+
+  const recordBtn = document.querySelector('#button')
+  const micBtn =  document.querySelector('#speech')
+  const waveBtn = document.querySelector('#mic')
+  const suggest = document.querySelector('#suggestion')
+  const loadBtn = document.querySelector('#load')
+  const speechToText = document.querySelector('#speechtotext')
+  let output = document.querySelector("#content");
+  const emotion =  document.querySelector("#emotion-text");
+  const emoji =  document.querySelector("#emoji");
+  
+/** Scroll on "Start Recording" button press on home button */
+  recordBtn.addEventListener('click', () => {
+    window.location.href = "#pricing"
+  })
+
+    /** Show wave, suggestions, loading on Mic Press */
+
+  micBtn.addEventListener('click', () => {
+    runSpeechRecognition();
+    waveBtn.style.visibility = "visible";
+    // suggest.style.visibility = "visible";
+    loadBtn.style.visibility = "visible";
+    speechToText.style.visibility = "visible";
+    output.style.visibility = "visible";
+    emotion.style.visibility = "visible";
+    emoji.style.visibility = "visible";
+  })
+
+  function runSpeechRecognition() {
+
+    const displayTxt = document.querySelector("#display-text");
+    // get output div reference
+    var output = document.querySelector("#content");
+    // get action element reference
+    var action = document.querySelector("#mic");
+        // new speech recognition object
+        var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+        var recognition = new SpeechRecognition();
+    
+        // This runs when the speech recognition service starts
+        recognition.onstart = function() {
+            displayTxt.innerHTML = "<small>listening, please speak <b>one</b> word at a time...</small>";
+        };
+        
+        recognition.onspeechend = function() {
+            displayTxt.innerHTML = "<small>stopped listening, detecting emotion...</small>";
+            recognition.stop();
+        }
+      
+        // This runs when the speech recognition service returns result
+        recognition.onresult = function(event) {
+            var transcript = event.results[0][0].transcript;
+            var confidence = event.results[0][0].confidence;
+            output.innerHTML = transcript;
+            output.classList.remove("hide");
+        };
+         // start recognition
+         recognition.start();
+  }
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+})()
